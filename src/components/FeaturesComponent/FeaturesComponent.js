14b4b044bc1faea687a7c28403b291cfd832e3a8
espinosa4-sofi <jespinosa@sofi.org>
2020-03-24 16:14:06 -0500

    creating components, styles, and more. Changing from next to react-script

---
+++ src/components/FeaturesComponent/FeaturesComponent.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './style.css';
+import 'simple-line-icons/css/simple-line-icons.css';
+import featureScreen from './img/2.png';
+
+export const FeaturesComponent = (props)=>{
+  return <section className="features" id="features">
+    <div className="container">
+      <div className="section-heading text-center">
+        <h2>Una herramienta para aumentar tus ventas</h2>
+        <p className="text-muted">Vende  a través de tu propia tienda virtual</p>
+        <hr />
+      </div>
+      <div className="row">
+        <div className="col-lg-4 my-auto">
+          <div className="device-container">
+            <div className="device-mockup iphone6_plus portrait white">
+              <div className="device">
+                <div className="screen">
+                  <img src={featureScreen} className="img-fluid" alt="" />
+                </div>
+                <div className="button">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-8 my-auto">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="feature-item">
+                  <i className="icon-screen-smartphone text-primary"></i>
+                  <h3>Ten visibilidad dígital</h3>
+                  <p className="text-muted">Un perfil digital visible para tus clientes</p>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="feature-item">
+                  <i className="icon-location-pin text-primary"></i>
+                  <h3>Acercate a personas de tu barrio</h3>
+                  <p className="text-muted">Personas en busca de tu negocio a máximo 2 kms de distancia</p>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="feature-item">
+                  <i className="icon-credit-card text-primary"></i>
+                  <h3>Recibe pagos con tarjetas</h3>
+                  <p className="text-muted">A través de tu tienda virtual en nuestra plataforma, podrás recibir pagos con tarjetas</p>
+                </div>
+              </div>
+              <div className="col-lg-6">
+                <div className="feature-item">
+                  <i className="icon-envelope-letter text-primary"></i>
+                  <h3>Chat para tu negocio</h3>
+                  <p className="text-muted">Permite que personas interesadas y clientes te contacten via chat</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+};
---
+++ src/components/FooterComponent/FooterComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export const FooterComponent = (props)=>{
+  return <footer>
+    <div className="container">
+      <p>&copy; 2020 TuTendero. Todos los derechos reservados.</p>
+      <ul className="list-inline">
+        <li className="list-inline-item">
+          <a href="#">Privacidad</a>
+        </li>
+        <li className="list-inline-item">
+          <a href="#">Términos y condiciones</a>
+        </li>
+        <li className="list-inline-item">
+          <a href="#">Preguntas frecuentes</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+};
