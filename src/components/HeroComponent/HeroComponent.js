38cb9197ec4278828d50d7ea5b0734104d6a37a6
espinosa4-sofi <jespinosa@sofi.org>
2020-04-11 12:23:01 -0500

    changing date

--- src/components/HeroComponent/HeroComponent.js
+++ src/components/HeroComponent/HeroComponent.js
@@ -11,7 +11,7 @@ export const HeroComponent = (props)=>{
         <div className="col-lg-7 my-auto">
           <div className="header-content mx-auto">
             <h1 className="mb-5">Permite que clientes cercanos te encuentren y te hagan pedidos</h1>
-            <h3 className="mb-5">Aplicación disponible desde el próximo 20 de abril. Totalmente gratis sólo para los primeros 100 tenderos por 6 meses </h3>
+            <h3 className="mb-5">Aplicación disponible desde el próximo 27 de abril. Totalmente gratis sólo para los primeros 100 tenderos por 6 meses </h3>
             <Link to="registro">
               <div className="btn btn-outline btn-xl js-scroll-trigger">Uneté Ahora</div>
             </Link>
